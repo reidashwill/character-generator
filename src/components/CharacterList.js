a234ef96c94b74d7a1a249d10eca1f6c34427a36
Reid Ashwill <reidashwill@gmail.com>
2020-07-24 14:25:08 -0700

    add reuasable form for creating and editing characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function CharacterList(){
   return(
