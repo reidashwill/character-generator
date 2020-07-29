9e14591292114244ead53843c8585ab514004b32
Reid Ashwill <reidashwill@gmail.com>
2020-07-29 09:39:31 -0700

    wrestle with flexbox to get the layout of character detail more in line with what is envisioned

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -21,71 +21,76 @@ function CharacterDetail(props) {
   ) 
   return ( 
     <React.Fragment>
-      <div className='genStats flexContainer'>
-        <h1>Name: {character.name}</h1>
-        <h3>Class: {character.characterClass}</h3>
-        <h3>Race: {character.race}</h3>
-        <h3>Alignment: {character.alignment} </h3>
-        {/* <h3>Level: {character.lvl}</h3> */}
-        <h3>Background: {character.background}</h3>
-      </div>
       
-      <div className='genStats flexContainer'>
-        <h3>Hit Points: {character.hp}</h3>
-        <h3>Hit Die: {character.hitDie}</h3>
-        <h3>Initiative: {character.initiative}</h3>
-        <h3>Speed: {character.speed}</h3>
-      </div>
+        <div className='genStats flexTopRow'>
+          <h1>Name: {character.name}</h1>
+          <h3>Class: {character.characterClass}</h3>
+          <h3>Race: {character.race}</h3>
+          <h3>Alignment: {character.alignment} </h3>
+          {/* <h3>Level: {character.lvl}</h3> */}
+          <h3>Background: {character.background}</h3>
+        </div>
+        <div className='charSheet'>
+        <div className='genStats flexCol'>
+          <h3>Acrobatics: {character.acrobatics}</h3>
+          <h3>Animal Handling: {character.animalHandling}</h3>
+          <h3>Arcana: {character.arcana}</h3>
+          <h3>Athletics: {character.athletics}</h3>
+          <h3>Deception: {character.deception}</h3>
+          <h3>History: {character.history}</h3>
+          <h3>Insight: {character.insight}</h3>
+          <h3>Intimidation: {character.intimidation} </h3>
+          <h3>Investigation: {character.investigation}</h3>
+          <h3>Medicine: {character.medicine}</h3>
+          <h3>Nature: {character.nature}</h3>
+          <h3>Perception: {character.perception}</h3>
+          <h3>Performance: {character.performance}</h3>
+          <h3>Persuasion: {character.persuasion}</h3>
+          <h3>Religion: {character.religion}</h3>
+          <h3>Sleight of Hand: {character.sleightOfHand}</h3>
+          <h3>Stealth: {character.stealth}</h3>
+          <h3>Survival: {character.survival}</h3>
+        </div>
+        <div className='genStats flexSubRow'>
+        <div className='genStats flexSubRow'>
+          <h3>Hit Points: {character.hp}</h3>
+          <h3>Hit Die: {character.hitDie}</h3>
+          <h3>Initiative: {character.initiative}</h3>
+          <h3>Speed: {character.speed}</h3>
+        </div>
 
-      <div className='genStats flexContainer'>
-        <h3>Strength: {character.str}</h3>
-        <h3>Dexterity: {character.dex}</h3>
-        <h3>Constitution {character.con}</h3>
-        <h3>Intelligence: {character.int}</h3>
-        <h3>Wisdom: {character.wis}</h3>
-        <h3>Charisma: {character.cha}</h3>
-      </div>
+        <div className='genStats flexSubRow'>
+          <h3>Strength: {character.str}</h3>
+          <h3>Dexterity: {character.dex}</h3>
+          <h3>Constitution {character.con}</h3>
+          <h3>Intelligence: {character.int}</h3>
+          <h3>Wisdom: {character.wis}</h3>
+          <h3>Charisma: {character.cha}</h3>
+        </div>
 
-      <div className='genStats flexContainer'>
-        <h3>Strength Modifier: {character.strMod}</h3>
-        <h3>Dexterity Modifier: {character.dexMod}</h3>
-        <h3>Constitution Modifier: {character.conMod}</h3>
-        <h3>Intelligence Modifier: {character.intMod}</h3>
-        <h3>Wisdom Modifier: {character.wisMod}</h3>
-        <h3>Charisma Modifier: {character.chaMod}</h3>
-      </div>
-      
-      <div className='genStats flexContainer'>
-        <h3>Acrobatics: {character.acrobatics}</h3>
-        <h3>Animal Handling: {character.animalHandling}</h3>
-        <h3>Arcana: {character.arcana}</h3>
-        <h3>Athletics: {character.athletics}</h3>
-        <h3>Deception: {character.deception}</h3>
-        <h3>History: {character.history}</h3>
-        <h3>Insight: {character.insight}</h3>
-        <h3>Intimidation: {character.intimidation} </h3>
-        <h3>Investigation: {character.investigation}</h3>
-        <h3>Medicine: {character.medicine}</h3>
-        <h3>Nature: {character.nature}</h3>
-        <h3>Perception: {character.perception}</h3>
-        <h3>Performance: {character.performance}</h3>
-        <h3>Persuasion: {character.persuasion}</h3>
-        <h3>Religion: {character.religion}</h3>
-        <h3>Sleight of Hand: {character.sleightOfHand}</h3>
-        <h3>Stealth: {character.stealth}</h3>
-        <h3>Survival: {character.survival}</h3>
-      </div>
+        <div className='genStats flexSubRow'>
+          <h3>Strength Modifier: {character.strMod}</h3>
+          <h3>Dexterity Modifier: {character.dexMod}</h3>
+          <h3>Constitution Modifier: {character.conMod}</h3>
+          <h3>Intelligence Modifier: {character.intMod}</h3>
+          <h3>Wisdom Modifier: {character.wisMod}</h3>
+          <h3>Charisma Modifier: {character.chaMod}</h3>
+        </div>
+        
+        
 
-      <div className='genStats flexContainer'>
-        <h3>Physical Appearance:</h3>
-        <p>{character.physicalApprearance}</p>
-        <h3>Personality Traits:</h3>
-        <p>{character.personalityTraits}</p>
-        <h3>Bonds and Flaws:</h3>
-        <p>{character.bondsFlaws}</p>
-      </div>
-      
-      <button onClick={ () => handleDeletingCharacter(selectedCharacter)}>Delete Character</button>
+        <div className='genStats flexSubRow'>
+          <h3>Physical Appearance:</h3>
+          <p>{character.physicalApprearance}</p>
+          <h3>Personality Traits:</h3>
+          <p>{character.personalityTraits}</p>
+          <h3>Bonds and Flaws:</h3>
+          <p>{character.bondsFlaws}</p>
+        </div>
+      </div>  
+      </div> 
+        <button onClick={ () => handleDeletingCharacter(selectedCharacter)}>Delete Character</button>
+       
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 function Header(){
   return (
     <React.Fragment>
-      <div className='flexContainer'>
+      <div className='flexTopRow'>
         <h1 className="header bungee">D&D CHARACTER VAULT</h1>
       </div>
-      <div className='flexContainer'>
+      <div className='flexTopRow'>
         <Link className='bungee navigation' to="/">Home</Link>
         <Link className='bungee navigation' to="/signin">Sign In</Link>
       </div>
